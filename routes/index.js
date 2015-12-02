ac5e7d7ba7e2c1ff1ddc4d20726112eb789692b5
bo <xuemaimai09281077@gmail.com>
2015-12-02 15:21:19 -0500

    sync code

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/user',function(req,res){
+console.log('bbbb',res.body)  
+res.json({msg:'Ok'})
+});
+
+
 module.exports = router;
